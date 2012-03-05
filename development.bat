a7c2b312191f15f25c293be241932f01c2b7a3fb
mihaibirsan <alexandru.m.birsan@gmail.com>
2012-03-05 12:45:12 +0200

    Switched Lighttpd batch file to rely on Environment Variables

--- development.bat
+++ development.bat
@@ -4,5 +4,5 @@ ECHO Press 'CTRL + C' to exit.
 ECHO.
 ECHO LightTPD Output:
 ECHO ----------------
-START /B C:\Progra~1\LightTPD\LightTPD.exe -f development.conf -m C:\Progra~1\LightTPD\lib -D
+START /B %LIGHTTPD_HOME%\LightTPD.exe -f development.conf -m %LIGHTTPD_HOME%\lib -D
 PAUSE >NUL && EXIT
